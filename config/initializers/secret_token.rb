b553102e7257efff24b7ff2f0d4cd0123d990228
hector adrian <hvaldecantos@gmail.com>
2013-04-15 13:45:10 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OnlineApps::Application.config.secret_token = '3c6e7e2dbc12b55850dc31d76bd387b8c1cd075eeb28aa75043ebd074cbb64b0ec807ab506c0942932a4e564fb789c4f250c8e1c216cc59be0541df6ed94a29c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+OnlineApps::Application.config.session_store :cookie_store, key: '_online_apps_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# OnlineApps::Application.config.session_store :active_record_store
