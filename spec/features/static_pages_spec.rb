7e0b9d4a734d432088f8b7667ddd15d79d28545d
Héctor Adrián Valdecantos <hvaldecantos@gmail.com>
2013-04-16 23:12:35 -0300

    Refactor specs for static pages

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -6,13 +6,13 @@ describe "Static pages" do
   it "should have the right links on the layout" do
     visit root_path
     click_link "About"
-    page.should have_title "Online App Container | About Us"
+    page.should have_title full_title("About Us")
     click_link "Help"
-    page.should have_title "Online App Container | Help"
+    page.should have_title full_title("Help")
     click_link "Contact"
-    page.should have_title "Online App Container | Contact"
+    page.should have_title full_title("Contact")
     click_link "Home"
-    page.should have_title "Online App Container"
+    page.should have_title full_title("")
   end
 
   subject { page }
@@ -21,24 +21,24 @@ describe "Static pages" do
     before { visit root_path } # to visit the root path before each exampl
     it { should have_selector('h1', text: 'Online APPs') }
     it { should have_title full_title("")}
-    it { should_not have_title "Online App Container | Home" }
+    it { should_not have_title full_title("Home") }
   end
 
   describe "Help page" do
     before { visit help_path }
     it { should have_selector('h1', text: 'Help') }
-    it { should have_title "Online App Container | Help" }
+    it { should have_title full_title("Help") }
   end
 
   describe "About page" do
     before { visit about_path }
     it { should have_selector('h1', text: 'About Us') }
-    it { should have_title "Online App Container | About Us" }
+    it { should have_title full_title("About Us") }
   end
 
   describe "Contact page" do
     before { visit contact_path }
     it { should have_selector('h1', text: 'Contact') }
-    it { should have_title "Online App Container | Contact" }
+    it { should have_title full_title("Contact") }
   end
 end
