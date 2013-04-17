01928453cceac3eeb6d9c064397dc4b4f58f84ff
Héctor Adrián Valdecantos <hvaldecantos@gmail.com>
2013-04-16 22:40:10 -0300

    Change routes URI name

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -5,17 +5,17 @@ describe "Static pages" do
   describe "Home page" do
 
     it "should have the content 'Online APPs'" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_selector('h1', text: 'Online APPs')
     end
 
     it "should have the base title" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_title "Online App Container"
     end
 
     it "should not have a custom page title" do
-      visit '/static_pages/home'
+      visit root_path
       page.should_not have_title "Online App Container | Home"
     end
   end
@@ -23,12 +23,12 @@ describe "Static pages" do
   describe "Help page" do
 
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_selector('h1', text: 'Help')
     end
 
     it "should have the title 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_title "Online App Container | Help"
     end
   end
@@ -36,12 +36,12 @@ describe "Static pages" do
   describe "About page" do
 
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_selector('h1', text: 'About Us')
     end
 
     it "should have the title 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_title "Online App Container | About Us"
     end
   end
@@ -49,12 +49,12 @@ describe "Static pages" do
   describe "Contact page" do
 
     it "should have the content 'Contact'" do
-      visit '/static_pages/contact'
+      visit contact_path
       page.should have_selector('h1', text: 'Contact')
     end
 
     it "should have the title 'About Us'" do
-      visit '/static_pages/contact'
+      visit contact_path
       page.should have_title "Online App Container | Contact"
     end
   end
